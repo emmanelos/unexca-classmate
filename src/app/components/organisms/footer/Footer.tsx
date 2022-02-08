526e086004f9a1010f6cb1222dc7232f2cd992f6
emmanelos <emmanelosg@gmail.com>
2022-02-08 01:21:45 -0400

    Create footer

--- src/app/components/organisms/footer/Footer.tsx
+++ src/app/components/organisms/footer/Footer.tsx
@@ -1,5 +1,16 @@
+import './Footer.scss';
 const Footer = () => {
-	return <footer className="footer">Footer</footer>;
+	return (
+		<footer className="footer">
+			<p className="footer__text">
+				Copyright © 2022{' '}
+				<a className="footer__link" href="http://www.unexca.edu.ve/">
+					Universidad Nacional Experimental de la Gran Caracas – UNEXCA
+				</a>
+				. Todos los derechos reservados.
+			</p>
+		</footer>
+	);
 };
 
 export default Footer;
