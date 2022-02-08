5885bad3c7c93ac1b1c92b1600851914da8e0cd3
emmanelos <emmanelosg@gmail.com>
2022-02-08 01:21:07 -0400

    Create header

--- src/app/components/organisms/header/Header.tsx
+++ src/app/components/organisms/header/Header.tsx
@@ -1,5 +1,14 @@
+import './Header.scss';
+
 const Header = () => {
-	return <header className="header">Header</header>;
+	return (
+		<header className="header">
+			<div className="header__menu">
+				<span className="header__hamburger"></span>
+			</div>
+			<h1 className="header__title">Unexca Classmate</h1>
+		</header>
+	);
 };
 
 export default Header;
