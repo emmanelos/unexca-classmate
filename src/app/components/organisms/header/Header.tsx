df6221d2ec5f9c3c92aa5d7d7ffb675f932f5993
emmanelos <emmanelosg@gmail.com>
2022-02-08 23:27:02 -0400

    Update home, header and footer

--- src/app/components/organisms/header/Header.tsx
+++ src/app/components/organisms/header/Header.tsx
@@ -1,16 +1,20 @@
 import './Header.scss';
 import encabezado from '../../../../assets/images/encabezado.png';
+import HomeContainer from '../../../pages/home/HomeContainer';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
 		<header className="header container">
-			<img className="header__unexca" src={encabezado} alt="" />
+			<img className="header__unexca" src={encabezado} alt="encabezado" />
 			<div className="header__bottom">
 				<div className="header__menu">
 					<span className="header__hamburger"></span>
 				</div>
 				<h1 className="header__title">
-					Unexca <p className="header__classmate">Classmate</p>
+					<Link to="/">
+						Unexca <p className="header__classmate">Classmate</p>
+					</Link>
 				</h1>
 			</div>
 		</header>
